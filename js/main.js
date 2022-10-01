b7f5948b77a6ac4aa0a0842d6cccd8d6da4d32bc
scsonic <scsonic@gmail.com>
2022-10-02 01:28:01 +0800

    Move all style js here

--- js/main.js
+++ js/main.js
@@ -10,3 +10,88 @@ var bind_all = function(){
         console.log("start search!!" + search_text) ;
     }) ;
 }
+
+
+let style_arr = ["images/style1.jpg", "images/style2.jpg", "images/style3.png", "images/style4.jpg"] ;
+let test_image = ["images/duck.jpg", "images/webb.jpg", "images/puppy.jpg", "images/baby.jpg"] ;
+
+var style_img = document.getElementById('style-img') ;
+var content_img = document.getElementById('content-img') ;
+var btnStart = document.getElementById('style-button');
+
+var style_end_callback = function(){ } ;
+
+function done(){
+    console.log("process done!") ;
+    
+    if (style_end_callback != undefined ) {
+        style_end_callback() ;  
+    }
+    
+    var img = document.createElement('img');
+    var sourceCanvas = document.getElementById("stylized");
+    var sourceImageData = sourceCanvas.toDataURL("image/png");
+    img.src = sourceImageData;
+
+
+    var results = document.getElementById("results");
+    results.appendChild(img);
+}
+
+
+var styleImage = function(content_url, style_url, callback) {
+
+    if ( callback != undefined ) {
+        style_end_callback = callback ;
+    }
+    style_img.onload = undefined ;
+    style_img.src = "" ;
+    style_img.onload = function(){
+        console.log("new style onloaded!") ;
+
+        content_img.onload = undefined ;
+        content_img.src = "" ;
+        content_img.onload = function(){
+            setTimeout(function(){
+                btnStart.onclick() ;
+            },100) ;
+            
+        }
+        content_img.src = content_url ;
+
+    } ;
+    style_img.src = style_url ;
+}
+
+var cnt = 10 ;
+var ii = 0 ;
+var jj = 0 ;
+
+function loopGen(){
+
+    if ( ii < style_arr.length) {
+        if ( jj < test_image.length ) {
+            console.log("start:" + style_arr[ii] + "," + test_image[jj]) ;
+            styleImage(test_image[jj], style_arr[ii], function(){
+                setTimeout(function(){
+                    loopGen() ;
+                }, 500) ;
+            })
+            jj ++ ;
+        }
+        else {
+            jj = 0 ;
+            ii ++ ;
+            loopGen() ;
+                var hr = document.createElement('hr');
+                document.body.appendChild(hr)
+        }
+    }
+}
+
+function test(){
+
+    ii = 0 ;
+    jj = 0 ;
+    loopGen() ;
+}
